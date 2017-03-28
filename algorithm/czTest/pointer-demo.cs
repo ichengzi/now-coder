d22670dae6b9cd348832023343a425a56afcdaf6
cz-lab-507 <chengzi12130@gmail.com>
2017-03-28 08:45:33 +0800

    up

--- algorithm/czTest/pointer-demo.cs
+++ algorithm/czTest/pointer-demo.cs
@@ -10,40 +10,40 @@ namespace czTest
     {
         public static void Main()
         {
-            // Normal pointer to an object.  
-            int[] a = new int[5] { 10, 20, 30, 40, 50 };
-            // Must be in unsafe code to use interior pointers.  
-            unsafe
-            {
-                // Must pin object on heap so that it doesn't move while using interior pointers.  
-                fixed (int* p = &a[0])
-                {
-                    // p is pinned as well as object, so create another pointer to show incrementing it.  
-                    int* p2 = p;
-                    Console.WriteLine(*p2);
-                    // Incrementing p2 bumps the pointer by four bytes due to its type ...  
-                    p2 += 1;
-                    Console.WriteLine(*p2);
-                    p2 += 1;
-                    Console.WriteLine(*p2);
-                    Console.WriteLine("--------");
-                    Console.WriteLine(*p);
-                    // Deferencing p and incrementing changes the value of a[0] ...  
-                    *p += 1;
-                    Console.WriteLine(*p);
-                    *p += 1;
-                    Console.WriteLine(*p);
+            //// Normal pointer to an object.  
+            //int[] a = new int[5] { 10, 20, 30, 40, 50 };
+            //// Must be in unsafe code to use interior pointers.  
+            //unsafe
+            //{
+            //    // Must pin object on heap so that it doesn't move while using interior pointers.  
+            //    fixed (int* p = &a[0])
+            //    {
+            //        // p is pinned as well as object, so create another pointer to show incrementing it.  
+            //        int* p2 = p;
+            //        Console.WriteLine(*p2);
+            //        // Incrementing p2 bumps the pointer by four bytes due to its type ...  
+            //        p2 += 1;
+            //        Console.WriteLine(*p2);
+            //        p2 += 1;
+            //        Console.WriteLine(*p2);
+            //        Console.WriteLine("--------");
+            //        Console.WriteLine(*p);
+            //        // Deferencing p and incrementing changes the value of a[0] ...  
+            //        *p += 1;
+            //        Console.WriteLine(*p);
+            //        *p += 1;
+            //        Console.WriteLine(*p);
 
 
-                    var xx = DateTime.Now;
-                    DateTime* date = &xx;
-                    Console.WriteLine(date->Millisecond);
-                }
-            }
+            //        var xx = DateTime.Now;
+            //        DateTime* date = &xx;
+            //        Console.WriteLine(date->Millisecond);
+            //    }
+            //}
 
-            Console.WriteLine("--------");
-            Console.WriteLine(a[0]);
-            Console.ReadLine();
+            //Console.WriteLine("--------");
+            //Console.WriteLine(a[0]);
+            //Console.ReadLine();
 
             // Output:  
             //10  
@@ -55,7 +55,13 @@ namespace czTest
             //12  
             //--------  
             //12  
-  
+
+            uint xxx = 5;
+            int yy = (int)xxx;
+
+            int b =0, c = 0;
+            uint a = b == c ? 0U : 1U;
+
         }
     }
 }
