a2965e09a82a7245f569c3679ca01c6f50ab4061
cz-lab-507 <chengzi12130@gmail.com>
2017-04-03 15:08:59 +0800

    .net remoting Demo

---
+++ dotnetRemoting/dotnetRemoting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace remoteServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemotingConfiguration.Configure("App.config", false);
+            Console.WriteLine("Listening for requests. Press enter to exit...");
+            Console.Read();
+        }
+    }
+
+    
+}
---
+++ dotnetRemoting/remotingClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace remotingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemotingConfiguration.Configure("App.config");
+            RemotableType.RemotableType remoteObject = new RemotableType.RemotableType();
+            Console.WriteLine(remoteObject.SayHello());
+            Console.Read();
+        }
+    }
+}
